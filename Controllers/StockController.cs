4eac3c9dd1f49533b50421e76b87a47292722209
Marcell Juhász <juhasz.marcell13@gmail.com>
2024-01-28 19:06:51 +0100

    endpoints added

---
+++ Controllers/StockController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalThinkersAssignment.Controllers
+{
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("/api/v1/Stock")]
+        public IActionResult Stock()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("/api/v1/Stock")]
+        public IActionResult GetCurrentStock() 
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("/api/v1/Checkout")]
+        public IActionResult Checkout()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
